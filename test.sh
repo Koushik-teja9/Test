2984ee76608377cf173afe651f2e1b9254e0eb30
Koushik-teja9 <koushikteja999@gmail.com>
2025-05-28 21:06:46 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+hi
+sudo su - for authentication failed
+inito    to close the machine
+rpm -qa   it shows what are the packages installed
+rpm -qa | grep httpd  for filteration of particular package
+rpm -ivh    like to download particular package
+rpm -ivh  i means installation
+          v means verbos
+          h means hashes
+rpm -e    to earse the installed package
+scp      move file from one server to another server
